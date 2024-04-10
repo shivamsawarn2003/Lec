d79f79431b0f272dd535ba65d528b5546e6a476a
Shivam <shivamsawarn2003@gmail.com>
2024-04-10 19:43:35 +0530

    useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,17 @@
 import './App.css';
-import {useState} from "react";
+ import {useState} from "react";
 
-
+import {Text} from "./Text";
 function App() {
 
-  const [todoList,settodoList]=useState([]);
-  const [newTask,setNewTask]=useState("");
+const [showText,setshowText]=useState(false);
 
-const handleChange=(event)=>{
-  setNewTask(event.target.value);
-}
-
-  const addTask=()=>{
-    const task={
-      id : todoList.length === 0 ? 1 : todoList[todoList.length-1].id+1,
-      taskName:newTask,
-      
-    }
-    settodoList([...todoList,task]) ; 
-  };
-  const deleteTask = (id) => {
-    settodoList(todoList.filter((task) => task.id !== id));
-  }
-  
   return (
     <div className="App">
-    <input onChange={handleChange}/>
-    <button onClick={addTask}>Add Task</button>
-
-   
-    <div className="list">
-
-      {todoList.map((task)=>{
-          return(<div> <h1>{task.taskName}</h1>
-          <button onClick={()=>deleteTask(task.id)}>X</button></div>)
-        })
-      }
-      </div> 
+<button onClick={()=>{
+  setshowText(!showText);
+}}>Show Text</button>
+{showText && <Text/>}
     </div>
   );
 }
