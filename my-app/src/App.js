2479067ff3fddd2fc7995a933dba2cfb9d0ba6b5
Shivam <shivamsawarn2003@gmail.com>
2024-04-17 21:12:23 +0530

    forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,12 @@
-import './App.css';
-import React from "react";
-import {useState,createContext}  from "react";
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
-import {Home} from "./components/Home"; 
-import {Profile} from "./components/Profile"; 
-import {Contact} from "./components/Contact"; 
-import {Navbar} from "./Navbar";
-
-export const AppContext=createContext();
-function App() {
-  const [username,setUsername]=useState("TechTimes");
-return (
+import "./App.css";
+import React from 'react'
+import {Form} from "./components/Form";
+const App = () => {
+  return (
     <div className="App">
-      <AppContext.Provider value={{username,setUsername}}>
-<Router>
-  <Navbar/>
-  <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/profile" element={<Profile/>}/>
-    <Route path="/contact" element={<Contact/>}/>
-    <Route path="*" element={<h1>Error 404</h1>}/> 
-  </Routes>
-</Router>
-</AppContext.Provider>
-
+      <Form/>
     </div>
-  );
+  )
 }
-export default App; 
+
+export default App
--- my-app/src/components/Contact.js
+++
@@ -1,9 +0,0 @@
-
-
-export const Contact = () => {
-    return (
-      <div>
-        Contact
-      </div>
-    )
-  }
